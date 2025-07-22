7a4029781591122c3e1eff6d339d7a4ca62df335
Lauren Malek <lauren.malek@berkeley.edu>
2025-07-22 15:39:41 -0700

    c

--- ollama_container/heatmap_generators/stl_evaluation.py
+++ ollama_container/heatmap_generators/stl_evaluation.py
@@ -166,7 +166,7 @@ sentences = translations['input statement'].unique()
 num_sentences = len(sentences)
 
 # Export {num_sentences}-many plots
-fig, axs = plt.subplots(num_sentences, 1, figsize=(100,10))
+fig, axs = plt.subplots(num_sentences, 1, figsize=(20,100))
 
 # These aggregate the data and labels for all the plots
 stl_matrix_arr = []
