4f3539209448899e9f309dd016d2ea8a3d420eb7
Lauren Malek <lauren.malek@berkeley.edu>
2025-07-25 09:37:10 -0700

    grammar explanation

--- ollama_container/stl_generators/stl_generator_v3.py
+++ ollama_container/stl_generators/stl_generator_v3.py
@@ -117,9 +117,7 @@ for sentence_index, sentence in sentences['input statement'].items():
             if translations.at[sentence_index, f'STL-{i}'] != 'STL could not be extracted':
                 translations.at[sentence_index, f'STL-{i}'] = "STL could not be parsed"
             syntactically_correct_responses.append(("STL could not be parsed", sentence_index, i))
-            logging.error(f'response is: {extracted_response}\nerror is: {e}\n')
             
-            # FORMAT THE ERROR MESSAGE IN A USEFUL WAY
             error_feedback = ''
             try:
                 error_message_less_descriptive = str(e).split('\n')[0].split(',')[0] 
