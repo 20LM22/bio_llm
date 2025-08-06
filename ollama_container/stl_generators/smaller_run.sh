21d6648aeec42f168a98afc094e42f99bd5c4391
Lauren Malek <lauren.malek@berkeley.edu>
2025-08-06 15:29:22 -0700

    updated smaller run to use new generator version

--- ollama_container/stl_generators/smaller_run.sh
+++ ollama_container/stl_generators/smaller_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python stl_generator_v5.py 'deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B' 'stl_generator_v4_config.json' > deepseek_convo.txt
+python stl_generator_v7.py 'deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B' 'stl_generator_v4_config.json' > deepseek_convo.txt
 python ../heatmap_generators/stl_evaluation_v6.py 'deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B' 'translations_DeepSeek-R1-Distill-Qwen-1.5B.pkl' 'stl_generator_v4_config.json'
-python stl_generator_v5.py 'Qwen/Qwen3-1.7B' 'stl_generator_v4_config.json' > qwen_convo.txt
+python stl_generator_v7.py 'Qwen/Qwen3-1.7B' 'stl_generator_v4_config.json' > qwen_convo.txt
 python ../heatmap_generators/stl_evaluation_v6.py 'Qwen/Qwen3-1.7B' 'translations_Qwen3-1.7B.pkl' 'stl_generator_v4_config.json'
