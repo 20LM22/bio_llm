02a2a1b8270e83466784387e99534e5bc03cb959
Lauren Malek <lauren.malek@berkeley.edu>
2025-08-08 03:09:08 -0700

    ran the semantic check on the rolledback version, yet to run the distributions

--- ollama_container/stl_generators/smaller_run.sh
+++ ollama_container/stl_generators/smaller_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # python stl_generator_v7.py 'deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B' 'stl_generator_v4_config.json' > deepseek_convo.txt
-python ../heatmap_generators/stl_evaluation_v6.py 'deepseek' 'translations_rollback_DeepSeek-R1-Distill-Qwen-1.5B.pkl' 'stl_generator_rollback_config.json' # will produce updated info on semantic attempts via sim_shot_semantic.csv files and improvements_all_sentences.csv
+python ../heatmap_generators/stl_evaluation_v6.py 'deepseek' 'translations_rollback_DeepSeek-R1-Distill-Qwen-1.5B.pkl' 'stl_generator_rollback_config.json'
 python ../heatmap_generators/generate_semantic_distribution_dict.py 'deepseek'
 python ../heatmap_generators/generate_semantic_distribution_plots.py 'deepseek'
 
@@ -8,3 +8,6 @@ python ../heatmap_generators/generate_semantic_distribution_plots.py 'deepseek'
 python ../heatmap_generators/stl_evaluation_v6.py 'qwen' 'translations_rollback_Qwen3-1.7B.pkl' 'stl_generator_rollback_config.json'
 python ../heatmap_generators/generate_semantic_distribution_dict.py 'qwen'
 python ../heatmap_generators/generate_semantic_distribution_plots.py 'qwen'
+
+# note: did not run the distributions on the rollback version yet
+# need to git commit this as "ran the semantic
