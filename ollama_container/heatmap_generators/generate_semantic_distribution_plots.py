612a11ff7c7091b814df6450b2a0393c3bf18224
Lauren Malek <lauren.malek@berkeley.edu>
2025-08-08 15:18:14 -0700

    fixed scale on histograms

--- ollama_container/heatmap_generators/generate_semantic_distribution_plots.py
+++ ollama_container/heatmap_generators/generate_semantic_distribution_plots.py
@@ -27,12 +27,14 @@ for key in res.keys():
             raise Exception(f'no choice associated with {stl}')
 
 # Plot histogram
+# bins = np.linspace(min(res[sentence]), max(res[sentence]), 10)
 bins = np.linspace(0, 1, 10) # change bin count if needed
 plt.hist(ones, bins=bins, alpha=0.5, label='Semantically correct', color='forestgreen', edgecolor='black')
 plt.hist(zeroes, bins=bins, alpha=0.5, label='Semantically incorrect', color='firebrick', edgecolor='black')
 
 plt.xlabel('Similarity')
 plt.ylabel('Frequency')
+plt.ylim(0, 50)
 plt.title(f'All Sentences Semantic Pass/Fail Distribution\nmodel: {model_name}')
 plt.legend()
 
@@ -41,12 +43,23 @@ plt.savefig(f"../images/{model_name}/histogram_distribution_comparison.png")
 # do them as sentences
 fig, axs = plt.subplots(len(res.keys()), 1, figsize=(20,60)) # TODO: change fig size if needed
 axs = np.atleast_1d(axs)
-bins = np.linspace(0, 1, 10) # change bin count if needed
 
 for _id, sentence in enumerate(res.keys()): # key is sentence
     zeroes = []
     ones = []
+    
+    max_sim = 0
+    min_sim = 0
+    first_time = True
+
     for stl, sim, choice in res[sentence]:
+        if first_time or sim > max_sim:
+            max_sim = sim
+            first_time = False
+        if first_time or sim < min_sim:
+            min_sim = sim
+            first_time = False
+
         if choice is None or choice == '':
             continue
         elif int(choice) == 0:
@@ -55,12 +68,16 @@ for _id, sentence in enumerate(res.keys()): # key is sentence
             ones.append(sim)
         else:
             raise Exception(f'no choice associated with {stl}')
+
+    bins = np.linspace(min_sim, max_sim, 10)
+
     axs[_id].hist(ones, bins=bins, alpha=0.5, label='Semantically correct', color='forestgreen', edgecolor='black')
     axs[_id].hist(zeroes, bins=bins, alpha=0.5, label='Semantically incorrect', color='firebrick', edgecolor='black')
 
     axs[_id].set_title(f'Semantic Pass/Fail Distribution\nsentence: {sentence}\nmodel: {model_name}')
     axs[_id].set_xlabel('Similarity')
     axs[_id].set_ylabel('Frequency')
+    axs[_id].set_ylim(0,15)
     axs[_id].legend()
 
 fig.tight_layout()
