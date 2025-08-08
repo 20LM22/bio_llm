d10343283df6aac6813e0e80bc878fb22f9b5f85
Lauren Malek <lauren.malek@berkeley.edu>
2025-08-08 03:02:27 -0700

    added extra rollback from when there was 1x semantic attempt at correction, yet to run

--- ollama_container/stl_generators/smaller_run.sh
+++ ollama_container/stl_generators/smaller_run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # python stl_generator_v7.py 'deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B' 'stl_generator_v4_config.json' > deepseek_convo.txt
-# python ../heatmap_generators/stl_evaluation_v6.py 'deepseek' 'translations_DeepSeek-R1-Distill-Qwen-1.5B.pkl' 'stl_generator_v4_config.json' # will produce updated info on semantic attempts via sim_shot_semantic.csv files and improvements_all_sentences.csv
+python ../heatmap_generators/stl_evaluation_v6.py 'deepseek' 'translations_rollback_DeepSeek-R1-Distill-Qwen-1.5B.pkl' 'stl_generator_rollback_config.json' # will produce updated info on semantic attempts via sim_shot_semantic.csv files and improvements_all_sentences.csv
 python ../heatmap_generators/generate_semantic_distribution_dict.py 'deepseek'
 python ../heatmap_generators/generate_semantic_distribution_plots.py 'deepseek'
 
 # python stl_generator_v7.py 'Qwen/Qwen3-1.7B' 'stl_generator_v4_config.json' > qwen_convo.txt
-python ../heatmap_generators/stl_evaluation_v6.py 'qwen' 'translations_Qwen3-1.7B.pkl' 'stl_generator_v4_config.json'
+python ../heatmap_generators/stl_evaluation_v6.py 'qwen' 'translations_rollback_Qwen3-1.7B.pkl' 'stl_generator_rollback_config.json'
 python ../heatmap_generators/generate_semantic_distribution_dict.py 'qwen'
 python ../heatmap_generators/generate_semantic_distribution_plots.py 'qwen'
