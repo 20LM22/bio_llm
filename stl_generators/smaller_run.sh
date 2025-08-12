01a9cb31d78eb3381be0d293561977f8429eda08
Lauren Malek <lauren.malek@berkeley.edu>
2025-08-12 12:03:38 -0700

    updated shell script for running new experiments

--- stl_generators/smaller_run.sh
+++ stl_generators/smaller_run.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
-config='config_shots_18_syntax_3_semantic_0.json'
-deepseek_convo='../stats/DeepSeek-R1-Distill-Qwen-1.5B/convo_shots_18_syntax_3_semantic_0.txt'
-qwen_convo='../stats/Qwen3-1.7B/convo_18_syntax_3_semantic_0.txt'
+deepseek = 'DeepSeek-R1-Distill-Qwen-1.5B'
+qwen = 'Qwen3-1.7B'
 
-#python stl_generator_v7.py 'deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B' $config > $deepseek_convo
-#python ../evaluations/stl_evaluation_v6.py 'DeepSeek-R1-Distill-Qwen-1.5B' 'translations_DeepSeek-R1-Distill-Qwen-1.5B_shots_18_syntax_3_semantic_0.pkl' $config
-#python ../evaluations/stl_evaluation_v6.py 'DeepSeek-R1-Distill-Qwen-1.5B' 'translations_DeepSeek-R1-Distill-Qwen-1.5B_shots_18_syntax_3_semantic_0.pkl' 'config_shots_18_syntax_3_semantic_0.json'
-#python ../evaluations/stl_evaluation_v6.py 'DeepSeek-R1-Distill-Qwen-1.5B' 'translations_DeepSeek-R1-Distill-Qwen-1.5B_shots_18_syntax_3_semantic_1.pkl' 'config_shots_18_syntax_3_semantic_1.json'
-#python ../evaluations/stl_evaluation_v6.py 'DeepSeek-R1-Distill-Qwen-1.5B' 'translations_DeepSeek-R1-Distill-Qwen-1.5B_shots_18_syntax_3_semantic_2.pkl' 'config_shots_18_syntax_3_semantic_2.json'
-#python ../evaluations/generate_semantic_distribution_dict.py 'DeepSeek-R1-Distill-Qwen-1.5B' 'translations_DeepSeek-R1-Distill-Qwen-1.5B_shots_18_syntax_3_semantic_0.pkl' $config
-#python ../evaluations/generate_semantic_distribution_plots.py 'DeepSeek-R1-Distill-Qwen-1.5B' $config
-python ../evaluations/generate_semantic_distribution_plots.py 'DeepSeek-R1-Distill-Qwen-1.5B' 'config_shots_18_syntax_3_semantic_0.json'
-python ../evaluations/generate_semantic_distribution_plots.py 'DeepSeek-R1-Distill-Qwen-1.5B' 'config_shots_18_syntax_3_semantic_1.json'
-python ../evaluations/generate_semantic_distribution_plots.py 'DeepSeek-R1-Distill-Qwen-1.5B' 'config_shots_18_syntax_3_semantic_2.json'
+experiments=("shots_18_syntax_1_semantic_2" "shots_18_syntax_2_semantic_2" "shots_18_syntax_4_semantic_2" "shots_5_syntax_3_semantic_2" "shots_100_syntax_3_semantic_2")
 
-#python stl_generator_v7.py 'Qwen/Qwen3-1.7B' $config > $qwen_convo
-#python ../evaluations/stl_evaluation_v6.py 'Qwen3-1.7B' 'translations_Qwen3-1.7B_shots_18_syntax_3_semantic_0.pkl' $config
-#python ../evaluations/stl_evaluation_v6.py 'Qwen3-1.7B' 'translations_Qwen3-1.7B_shots_18_syntax_3_semantic_0.pkl' 'config_shots_18_syntax_3_semantic_0.json'
-#python ../evaluations/stl_evaluation_v6.py 'Qwen3-1.7B' 'translations_Qwen3-1.7B_shots_18_syntax_3_semantic_1.pkl' 'config_shots_18_syntax_3_semantic_1.json'
-#python ../evaluations/stl_evaluation_v6.py 'Qwen3-1.7B' 'translations_Qwen3-1.7B_shots_18_syntax_3_semantic_2.pkl' 'config_shots_18_syntax_3_semantic_2.json'
-#python ../evaluations/generate_semantic_distribution_dict.py 'Qwen3-1.7B' 'translations_Qwen3-1.7B_shots_18_syntax_3_semantic_0.pkl' $config
-#python ../evaluations/generate_semantic_distribution_plots.py 'Qwen3-1.7B' $config
-python ../evaluations/generate_semantic_distribution_plots.py 'Qwen3-1.7B' 'config_shots_18_syntax_3_semantic_0.json'
-python ../evaluations/generate_semantic_distribution_plots.py 'Qwen3-1.7B' 'config_shots_18_syntax_3_semantic_1.json'
-python ../evaluations/generate_semantic_distribution_plots.py 'Qwen3-1.7B' 'config_shots_18_syntax_3_semantic_2.json'
+for experiment in experiments
+do
+  config="config_${experiment}.json"
+  deepseek_convo="../stats/DeepSeek-R1-Distill-Qwen-1.5B/config_${experiment}.txt"
+  qwen_convo="../stats/Qwen3-1.7B/config_${experiment}.txt"
+  translations_deepseek="translations_${deepseek}_${experiment}.pkl"
+  translations_qwen="translations_${qwen}_${experiment}.pkl"
+
+  python stl_generator_v7.py 'deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B' $config > $deepseek_convo
+  python ../evaluations/stl_evaluation_v6.py $deepseek $translations_deepseek $config
+  python ../evaluations/generate_semantic_distribution_dict.py $deepseek $translations_deepseek $config
+  python ../evaluations/generate_semantic_distribution_plots.py $deepseek $config
+
+  python stl_generator_v7.py 'Qwen/Qwen3-1.7B' $config > $qwen_convo
+  python ../evaluations/stl_evaluation_v6.py $qwen $translations_qwen $config
+  python ../evaluations/generate_semantic_distribution_dict.py $qwen $translations_qwen $config
+  python ../evaluations/generate_semantic_distribution_plots.py $qwen $config
+
+  echo "Done generating: ${experiment}"
+  git add .
+  git commit -m "Runs, evals with ${experiment}"
+  git push origin
+  echo "Pushed ${experiment}"
+done
