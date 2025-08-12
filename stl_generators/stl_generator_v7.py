8a477bc3ad5bb8aca4722a3bc4bc425148a3067c
Lauren Malek <lauren.malek@berkeley.edu>
2025-08-12 16:14:51 -0700

    changing file args

--- stl_generators/stl_generator_v7.py
+++ stl_generators/stl_generator_v7.py
@@ -581,9 +581,10 @@ for sentence_index, sentence in sentences['input statement'].items():
 
 # writing to pkl
 try:
-    with open(f'../pkl/{model_name}/all_responses_all_sentences_{model_name}.pkl', 'wb') as r:
+    config = sys.argv[2].split("config_")
+    with open(f'../pkl/{model_name}/all_responses_all_sentences_{model_name}_{config}.pkl', 'wb') as r:
         pickle.dump(all_responses_all_sentences, r)
-    with open(f'../pkl/{model_name}/translations_{model_name}.pkl', 'wb') as r:
+    with open(f'../pkl/{model_name}/translations_{model_name}_{config}.pkl', 'wb') as r:
         pickle.dump(translations, r)
 except Exception as e:
     print("there was a pickle problem")
