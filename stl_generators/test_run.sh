34b330e0b1d9bacbf88e2bff0416f0f5945d0ca1
Lauren Malek <lauren.malek@berkeley.edu>
2025-08-13 20:02:05 -0700

    Runs, evals with shots_50_syntax_3_semantic_2

---
+++ stl_generators/test_run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+deepseek='DeepSeek-R1-Distill-Qwen-1.5B'
+qwen='Qwen3-1.7B'
+
+experiments=("shots_18_syntax_3_semantic_0" "shots_18_syntax_3_semantic_1" "")
+
+for experiment in "${experiments[@]}"
+do
+  config="config_${experiment}.json"
+  echo $config
+  deepseek_convo="../stats/DeepSeek-R1-Distill-Qwen-1.5B/config_${experiment}.txt"
+  qwen_convo="../stats/Qwen3-1.7B/config_${experiment}.txt"
+  translations_deepseek="translations_${deepseek}_${experiment}.pkl"
+  translations_qwen="translations_${qwen}_${experiment}.pkl"
+
+  python stl_generator_v7.py 'deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B' $config $experiment > $deepseek_convo
+  python ../evaluations/stl_evaluation_v6.py $deepseek $translations_deepseek $config
+#  python ../evaluations/generate_semantic_distribution_dict.py $deepseek $translations_deepseek $config
+#  python ../evaluations/generate_semantic_distribution_plots.py $deepseek $config
+
+  python stl_generator_v7.py 'Qwen/Qwen3-1.7B' $config $experiment > $qwen_convo
+  python ../evaluations/stl_evaluation_v6.py $qwen $translations_qwen $config
+#  python ../evaluations/generate_semantic_distribution_dict.py $qwen $translations_qwen $config
+#  python ../evaluations/generate_semantic_distribution_plots.py $qwen $config
+
+  echo "Done generating: ${experiment}"
+  git add ..
+  git commit -m "Runs, evals with ${experiment}"
+  git push origin
+  echo "Pushed ${experiment}"
+done
