d296f15371cefdfbc0e3a336674e13eb89552422
Lauren Malek <lauren.malek@berkeley.edu>
2025-08-21 09:40:36 -0700

    fixed config file

--- stl_generators/run_gpt.sh
+++ stl_generators/run_gpt.sh
@@ -2,9 +2,7 @@
 gpt='gpt-4o-2024-08-06'
 
 # check that there's 8 things listed here
-experiments=("shots_5_syntax_3_semantic_2")
-
-# "shots_18_syntax_1_semantic_2" "shots_18_syntax_2_semantic_2" "shots_18_syntax_3_semantic_2" "shots_18_syntax_4_semantic_2" "shots_5_syntax_3_semantic_2" "shots_18_syntax_3_semantic_0" "shots_18_syntax_3_semantic_1" "shots_50_syntax_3_semantic_2")
+experiments=("shots_18_syntax_1_semantic_2" "shots_18_syntax_2_semantic_2" "shots_18_syntax_3_semantic_2" "shots_18_syntax_4_semantic_2" "shots_18_syntax_3_semantic_0" "shots_18_syntax_3_semantic_1")
 
 for experiment in "${experiments[@]}"
 do
@@ -12,6 +10,5 @@ do
   gpt_convo="../stats/${gpt}/convo_${experiment}.txt"
   echo $config
   translations_gpt="translations_${gpt}_${experiment}.pkl"
-  python stl_generator_v7.py 'gpt-4o-2024-08-06' "gpt_config_shots_5_syntax_3_semantic_2.json" "shots_5_syntax_3_semantic_2"
-  # > $deepseek_convo
+  python stl_generator_gpt.py $gpt $config $experiment > $gpt_convo
 done
