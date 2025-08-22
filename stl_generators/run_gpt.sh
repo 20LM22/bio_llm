5c0f2b3dd5e7e2c3c76c2fd1346dbecceca47e05
Lauren Malek <lauren.malek@berkeley.edu>
2025-08-22 01:21:37 -0400

    added gpt test histograms, 3 hi 3 lo

--- stl_generators/run_gpt.sh
+++ stl_generators/run_gpt.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 gpt='gpt-4o-2024-08-06'
 
-# check that there's 8 things listed here
-experiments=("shots_18_syntax_2_semantic_1")
+experiments=("shots_18_syntax_1_semantic_2" "shots_18_syntax_2_semantic_2" "shots_18_syntax_3_semantic_2" "shots_18_syntax_4_semantic_2" "shots_18_syntax_3_semantic_1")
 
 for experiment in "${experiments[@]}"
 do
-  config="gpt_test_set_config_${experiment}.json"
-  gpt_convo="../stats/${gpt}/test_set_convo_${experiment}.txt"
+  config="gpt_config_${experiment}.json"
+  gpt_convo="../stats/${gpt}/redo_ablation_convo_${experiment}.txt"
   echo $config
-  translations_gpt="test_set_translations_${gpt}_${experiment}.pkl"
+  translations_gpt="translations_${gpt}_${experiment}.pkl"
   python stl_generator_gpt.py $gpt $config $experiment >| $gpt_convo
   python ../evaluations/stl_evaluation_v6.py $gpt $translations_gpt $config
 done
--- stl_generators/run_gpt_annotations.sh
+++ stl_generators/run_gpt_annotations.sh
@@ -6,13 +6,11 @@ experiments=("shots_18_syntax_2_semantic_1")
 
 for experiment in "${experiments[@]}"
 do
-  config="gpt_config_${experiment}.json"
-  # gpt_convo="../stats/${gpt}/test_set_convo_${experiment}.txt"
+  config="gpt_test_set_config_${experiment}.json"
+
   echo $config
-  translations_gpt="translations_${gpt}_${experiment}.pkl"
-  # python stl_generator_gpt.py $gpt $config $experiment >| $gpt_convo
-  # python ../evaluations/stl_evaluation_v6.py $gpt $translations_gpt $config
-  # python ../evaluations/generate_semantic_distribution_dict.py $gpt $translations_gpt $config
-  python ../evaluations/generate_semantic_distribution_plots.py $gpt $config
-  python ../evaluations/generate_semantic_distribution_plots.py 'gpt-4o-2024-08-06' "gpt_config_shots_18_syntax_2_semantic_1.json"
+  translations_gpt="test_set_translations_${gpt}_${experiment}.pkl"
+
+  python ../evaluations/generate_semantic_distribution_dict.py $gpt $translations_gpt $config
+  python ../evaluations/generate_semantic_distribution_plots.py 'gpt-4o-2024-08-06' "gpt_test_set_config_shots_18_syntax_2_semantic_1.json"
 done
