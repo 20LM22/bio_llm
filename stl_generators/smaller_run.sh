d553dffa48525bab884cc90f4cf5b5d3ddee311a
Lauren Malek <lauren.malek@berkeley.edu>
2025-08-21 22:41:31 -0400

    now ready to run on gpu

--- stl_generators/smaller_run.sh
+++ stl_generators/smaller_run.sh
@@ -7,13 +7,13 @@ config="qd_test_set_config_${experiment}.json"
 echo $config
 
 qwen_convo="../stats/${qwen}/test_set_convo_${experiment}.txt"
-deepseek_convo_convo="../stats/${deepseek}/test_set_convo_${experiment}.txt"
+deepseek_convo="../stats/${deepseek}/test_set_convo_${experiment}.txt"
 
 translations_deepseek="test_set_translations_${deepseek}_${experiment}.pkl"
 translations_qwen="test_set_translations_${qwen}_${experiment}.pkl"
 
-python stl_generator_v7.py $deepseek $config $experiment >| $deepseek_convo # gpt is using the gpt generator
-python stl_generator_v7.py $qwen $config $experiment >| $qwen_convo # gpt is using the gpt generator
+python stl_generator_v7.py 'deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B' $config $experiment >| $deepseek_convo # gpt is using the gpt generator
+python stl_generator_v7.py 'Qwen3/Qwen3-1.7B' $config $experiment >| $qwen_convo # gpt is using the gpt generator
 
 python ../evaluations/stl_evaluation_v6.py $deepseek $translations_deepseek $config
 python ../evaluations/stl_evaluation_v6.py $qwen $translations_qwen $config
