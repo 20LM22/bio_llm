eadee95c1fb0972d9faa5cc173307ad6831308fd
Lauren Malek <lauren.malek@berkeley.edu>
2025-09-21 10:05:46 -0700

    prelim on deepseek run

--- stl_generators/run_all_non_gpt.sh
+++ stl_generators/run_all_non_gpt.sh
@@ -11,16 +11,16 @@ do
   config="config_${experiment}_${set_name}.json"
   echo $config
 
-  i=0
+  i=1 #TODO switch back to 0 when needed
   for model in "${models[@]}"
     do
-      time=$(date+%F_%T)
+      time=$(/usr/bin/date +%F_%T)
       convo="../stats/${model}/config_${experiment}_${time}.txt"
       translations="${set_name}_translations_${model}_${experiment}_${time}.pkl"
       python stl_generator_v7.py ${official_model_names[i]} $config $experiment $time >| $convo
 #      python ../evaluations/stl_evaluation_v6.py $model $translations $config $time
 #      python ../evaluations/generate_semantic_distribution_dict.py $model $translations $config $time
 #      python ../evaluations/generate_semantic_distribution_plots.py $model $config $time
-      i=i+1
+      ((i++))
     done
 done
