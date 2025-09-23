280ee436594ec6984d11d83fea92919c90e3490b
Lauren Malek <lauren.malek@berkeley.edu>
2025-09-22 22:37:15 -0700

    evals run

--- stl_generators/run_all_gpt.sh
+++ stl_generators/run_all_gpt.sh
@@ -14,11 +14,11 @@ do
   i=0
   for model in "${models[@]}"
     do
-      time=$(date +%F_%T)
+      time=2025-09-22_11:13:43 #$(date +%F_%T)
       convo="../stats/${model}/config_${experiment}_${time}.txt"
       translations="${set_name}_translations_${model}_${experiment}_${time}.pkl"
-      python stl_generator_gpt.py ${official_model_names[i]} $config $experiment $time >| $convo
-#      python ../evaluations/stl_evaluation_v6.py $model $translations $config $time
+#      python stl_generator_gpt.py ${official_model_names[i]} $config $experiment $time >| $convo
+      python ../evaluations/stl_evaluation_v6.py $model $translations $config $time
 #      python ../evaluations/generate_semantic_distribution_dict.py $model $translations $config $time
 #      python ../evaluations/generate_semantic_distribution_plots.py $model $config $time
       i=i+1
--- stl_generators/run_all_non_gpt.sh
+++ stl_generators/run_all_non_gpt.sh
@@ -14,12 +14,12 @@ do
   i=0 #TODO switch back to 0 when needed
   for model in "${models[@]}"
     do
-      time=$(/usr/bin/date +%F_%T)
+      time=2025-09-22_10:33:05 #$(/usr/bin/date +%F_%T)
       convo="../stats/${model}/config_${experiment}_${time}.txt"
       translations="${set_name}_translations_${model}_${experiment}_${time}.pkl"
       echo $config
-      python stl_generator_v7.py ${official_model_names[i]} $config $experiment $time >| $convo
-#      python ../evaluations/stl_evaluation_v6.py $model $translations $config $time
+#      python stl_generator_v7.py ${official_model_names[i]} $config $experiment $time >| $convo
+      python ../evaluations/stl_evaluation_v6.py $model $translations $config $time
 #      python ../evaluations/generate_semantic_distribution_dict.py $model $translations $config $time
 #      python ../evaluations/generate_semantic_distribution_plots.py $model $config $time
       ((i++))
