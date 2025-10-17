16cd4d7366ff291689bd371cbd30f651a99be5af
Lauren Malek <lauren.malek@berkeley.edu>
2025-10-17 15:08:28 -0400

    gpt scale change

--- evaluations/generate_semantic_distribution_plots.py
+++ evaluations/generate_semantic_distribution_plots.py
@@ -118,7 +118,7 @@ for _id, sentence in enumerate(res.keys()): # key is sentence
 
     plt.xlabel('Similarity')
     plt.ylabel('Frequency')
-    plt.ylim(0, 60)
+    plt.ylim(0, 45)
     plt.title(
         f'Hi/Lo Similarity Distribution with Semantic Correctness Labels\nmodel: {model_name}\nnx: {syntax_count}, ny: {semantic_count}, nz: {shot_count}, median: {median}\nsentence: {sentence[0:10]}')
     plt.savefig(
